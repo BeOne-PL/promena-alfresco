5138c6df9ee0b333e695360edfc4c230e18747c7
Bartosz Skotarek <bzskotarek@gmail.com>
2020-01-16 12:00:02 +0100

    [improved][alfresco-promena-connector-http] Enriched documentation

--- connector/alfresco-promena-connector-http/src/main/kotlin/pl/beone/promena/alfresco/module/connector/http/external/HttpPromenaTransformationExecutor.kt
+++ connector/alfresco-promena-connector-http/src/main/kotlin/pl/beone/promena/alfresco/module/connector/http/external/HttpPromenaTransformationExecutor.kt
@@ -38,7 +38,24 @@ import java.util.concurrent.Executors
  * Provides HTTP bridge between Alfresco Content Services and Promena.
  * This implementation uses `promena-connector-http` connector module so it's required to include its on Promena.
  *
- * @see HttpPromenaTransformer.execute
+ * The flow:
+ * 1. Validates [PostTransformationExecutor]
+ * 2. Checks if nodes from [NodeDescriptor] exist
+ * 3. Converts [NodeDescriptor] to [DataDescriptor]
+ * 4. Adds [PROPERTY_EXECUTION_IDS] to nodes from [NodeDescriptor]
+ * 5. Performs a transaction asynchronously
+ * 6. Returns [TransformationExecution]
+ *
+ * The flow of asynchronous transaction execution:
+ * 1. Performs a transformation on Promena ([HttpPromenaTransformer.execute])
+ * 2. Verifies if nodes from [NodeDescriptor] still exist
+ * 3. Checks if the checksum of nodes from [NodeDescriptor] hasn't changed
+ * 4. Saves the results of transformation execution ([transformedDataDescriptorSaver])
+ * 5. Executes [PostTransformationExecutor] if set
+ * 6. Cleans data ([dataCleaner])
+ * 7. Completes the transformation execution with the result ([promenaMutableTransformationManager])
+ * 8. In case of an error, another execution is run until the number of [Retry.maxAttempts] is reached.
+ *    If the number is reached, it completes the transformation execution with an exception ([promenaMutableTransformationManager])
  */
 class HttpPromenaTransformationExecutor(
     threads: Int,
